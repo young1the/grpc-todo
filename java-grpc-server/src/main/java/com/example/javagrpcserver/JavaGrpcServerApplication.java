d66d197f8fe28a74823b7bdff6bef077ce2c2e1e
young1the <youngthedev@gmail.com>
2023-12-11 09:44:47 +0900

    init: spring boot 3.1.4 grpc-server

---
+++ java-grpc-server/src/main/java/com/example/javagrpcserver/JavaGrpcServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagrpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGrpcServerApplication.class, args);
+    }
+
+}
