0e2174ea1a3901578515e46f2324c034ef615f83
young1the <youngthedev@gmail.com>
2023-12-11 17:18:57 +0900

    feat: grpc clients CREATE

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import cors from "cors"
 import { credentials } from "@grpc/grpc-js";
-import { GetTodosResponse, TodoServiceClient } from "./proto/todo";
+import { GetTodosResponse, SaveTodoRequest, TodoServiceClient } from "./proto/todo";
 
 const app = express()
 app.use(cors());
+app.use(express.json())
 const port = 3001
 
 const grpcClient = new TodoServiceClient(
@@ -18,8 +19,14 @@ app.get('/api/todo', async (_, res) => {
     })
 })
 
-// app.get('/api/todo', async (_, res) => {
-// })
+app.post('/api/todo', async (req, _) => {
+    console.log(req.body);
+    const message = { title: req.body.title ?? "" }
+    grpcClient.saveTodo(message, (error, reply) => {
+        console.log(reply);
+        console.log(error);
+    })
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
