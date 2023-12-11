edcb7951ce6c4d0f3fb3c9d6de4d4e8b268ce65e
young1the <youngthedev@gmail.com>
2023-12-11 11:47:48 +0900

    feat: express grpc client

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -1,10 +1,20 @@
 import express from "express"
+import { HelloReply, HelloRequest, MyServiceClient } from "./proto/hello";
+import { credentials } from "@grpc/grpc-js";
 
 const app = express()
 const port = 3001
 
-app.get('/', (_, res) => {
-    res.send('Hello World!')
+const grpcClient = new MyServiceClient(
+    'localhost:9090',
+    credentials.createInsecure()
+);
+
+app.get('/', async (_, res) => {
+    const message: HelloRequest = { name: "young" };
+    grpcClient.sayHello(message, (error, reply: HelloReply) => {
+        res.send(reply)
+    });
 })
 
 app.listen(port, () => {
