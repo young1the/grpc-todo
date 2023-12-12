d17917a64e13856c34a140009fedc0be7f1bdea7
young1the <youngthedev@gmail.com>
2023-12-12 14:00:31 +0900

    move: typescript

--- java-grpc-server/src/main/java/com/example/javagrpcserver/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.javagrpcserver.repository;
-
-import com.example.javagrpcserver.entity.TodoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
-}
